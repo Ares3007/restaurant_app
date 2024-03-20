095f7352af9442c60bd8a139f76582bf35fa160d
Ares3007 <ares.d@appwrk.com>
2024-03-20 18:22:36 +0530

    worked on qr code section

--- frontend/src/Pages/Menu.js
+++ frontend/src/Pages/Menu.js
@@ -3,14 +3,14 @@ import QRCode from "react-qr-code";
 
 function Menu() {
   const generateQRCodeValue = () => {
-    return "http://localhost:8000";
+    return "http://192.168.1.57:3000/user-order";
   };
 
   return (
     <div>
-      <h1>Search food from your mobile</h1>
-      <br />
-      <QRCode value={generateQRCodeValue()} />
+      <h1 style={{marginLeft:"400px"}}>Scan QR Code to Access Menu</h1>
+      <p style={{marginLeft:"400px"}}>Scan the QR code below to view the menu on your mobile device:</p>
+      <QRCode style={{margin:"100px", marginLeft:"400px"}} value={generateQRCodeValue()} />
     </div>
   );
 }
--- frontend/src/Pages/UserOrder.js
+++ frontend/src/Pages/UserOrder.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../css/UserOrder.css"; // Import the CSS file
+import "../css/UserOrder.css";
 
 function UserOrder() {
   const [menuData, setMenuData] = useState([]);
@@ -8,13 +8,13 @@ function UserOrder() {
   const [formData, setFormData] = useState({
     name: "",
     price: "",
-    category: ""
+    category: "",
   });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:8000/menu/all-menu`);
+        const response = await axios.get(`http://192.168.1.57:8000/menu/all-menu`);
         setMenuData(response.data);
       } catch (error) {
         console.error("Error in fetching data:", error);
@@ -30,22 +30,31 @@ function UserOrder() {
       if (updatedSelectedItems[itemId]) {
         delete updatedSelectedItems[itemId];
       } else {
-        updatedSelectedItems[itemId] = true;
+        updatedSelectedItems[itemId] = {
+          ...menuData.find((item) => item._id === itemId),
+          quantity: 1, 
+        };
       }
       return updatedSelectedItems;
     });
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (e, itemId) => {
+    const { value } = e.target;
+    setSelectedItems((prevSelectedItems) => {
+      const updatedSelectedItems = { ...prevSelectedItems };
+      updatedSelectedItems[itemId].quantity = parseInt(value) || 0;
+      return updatedSelectedItems;
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const selectedMenuItems = menuData.filter(item => selectedItems[item._id]);
-    console.log(selectedMenuItems);
-    setSelectedItems({});
   };
+
+  const totalBill = Object.values(selectedItems).reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
 
   const groupedMenuItems = {};
   menuData.forEach((menuItem) => {
@@ -57,40 +66,51 @@ function UserOrder() {
   });
 
   return (
-    <div className="menu-container">
-      <h2>Menu</h2>
-      {Object.keys(groupedMenuItems).map((categoryName) => (
-        <div key={categoryName} className="menu-category">
-          <h3>{categoryName}</h3>
-          {groupedMenuItems[categoryName].map((menuItem) => (
-            <div key={menuItem._id} className="menu-item">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedItems[menuItem._id]}
-                  onChange={() => handleCheckboxChange(menuItem._id)}
-                />
-                {menuItem.name}
-              </label>
+    <>
+      <h1 style={{ marginLeft: "500px" }}>Choose from wide range</h1>
+      <div className="menu-container">
+        <div>
+          {Object.keys(groupedMenuItems).map((categoryName) => (
+            <div key={categoryName} className="menu-category">
+              <h3>{categoryName}</h3>
+              {groupedMenuItems[categoryName].map((menuItem) => (
+                <div key={menuItem._id} className="menu-item">
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={!!selectedItems[menuItem._id]}
+                      onChange={() => handleCheckboxChange(menuItem._id)}
+                    />
+                    <div className="menu-item-details">
+                      <h3>{menuItem.name}</h3>
+                      <p>$ {menuItem.price}</p>
+                    </div>
+                  </label>
+                </div>
+              ))}
             </div>
           ))}
         </div>
-      ))}
-      <h2 className="selected-items">Selected Items</h2>
-      <form onSubmit={handleSubmit}>
-        {Object.values(selectedItems).map((isSelected, index) => (
-          <div key={index} className="selected-item">
-            <label>Selected Item {index + 1}</label>
-            <input
-              type="text"
-              value={isSelected ? `Item ${index + 1}` : ''}
-              onChange={(e) => handleChange(e, index)}
-            />
-          </div>
-        ))}
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+        <div>
+          <h2 className="selected-items">Total Bill</h2>
+          <form onSubmit={handleSubmit}>
+            {Object.values(selectedItems).map((item, index) => (
+              <div key={index} className="selected-item">
+                <label>* {item.name}</label>
+                <span>Price: ${item.price}</span>
+                <input
+                  type="number"
+                  value={item.quantity}
+                  onChange={(e) => handleChange(e, item._id)}
+                />
+              </div>
+            ))}
+            <div>Total: ${totalBill}</div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
